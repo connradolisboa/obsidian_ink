36b28e5f0912b53b02e4e61d7369ac15929f7131
Connrado Lisboa <connradolisboa@gmail.com>
2026-09-14 14:08:13 -0300

    Squashed 'companion-app/' content from commit 8d383e7
    git-subtree-dir: companion-app
    git-subtree-split: 8d383e793aec9946858f69ad01d6d973da06b091

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.spotless)
+}
+
+spotless {
+    kotlin {
+        target("**/src/**/*.kt")
+        ktlint()
+            .editorConfigOverride(
+                mapOf(
+                    "ktlint_code_style" to "android_studio",
+                )
+            )
+    }
+}
+
+android {
+    namespace = "com.sergeylappo.booxrapiddraw"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sergeylappo.booxrapiddraw"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 7
+        versionName = "0.0.7-alpha"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        ndk {
+            //abiFilters += "armeabi-v7a", "arm64-v8a", "x86", "x86_64"
+            abiFilters += setOf("armeabi-v7a", "arm64-v8a")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+
+        jniLibs {
+            pickFirsts += "lib/*/libc++_shared.so"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.fragment)
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.window)
+    implementation(libs.bundles.onyx)
+    implementation(libs.hiddenapibypass)
+}
